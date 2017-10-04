9dbcecc0db3a4603d6fd8c4856dfccd826229a4c
KaitieFox <kaitiefox@gmail.com>
2017-10-04 09:58:52 -0400

    Initial commit

---
+++ week2day3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for(int i = 10; i > 0; i-- ) //this is counting backwards.
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+
+
+        }
+    }
+}
